ee8793ee7c00545fd1141fa2436ff54c73232f10
rasooltaghipoor <rasool.taghipoor@gmail.com>
2023-08-26 12:42:16 +0330

    Spawn initial enemies

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -4,16 +4,33 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject[] _enemyPrefabs;
+    private const int _enemyPoolSize = 30;
+    private GameObject[] _enemyPool;
+    private float _lastYPos;
     // Start is called before the first frame update
     void Start()
     {
-
+        _enemyPool = new GameObject[_enemyPoolSize];
+        spawnInitialEnemies();
+        _lastYPos = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+
+    private void spawnInitialEnemies()
+    {
+        for (int i = 0; i < _enemyPoolSize; i++)
+        {
+            _enemyPool[i] = Instantiate(_enemyPrefabs[Random.Range(0, _enemyPrefabs.Length)]) as GameObject;
+            _lastYPos = Random.Range(_lastYPos - GameConrtoller.WorldScreenHeight / 2, _lastYPos - GameConrtoller.WorldScreenHeight * 2);
+            _enemyPool[i].transform.position = new Vector3(Random.Range(-GameConrtoller.WorldScreenWidth * 0.75f, GameConrtoller.WorldScreenWidth * 0.75f), _lastYPos, 0);
+        }
     }
 
 
--- Assets/Scripts/GameConrtoller.cs
+++ Assets/Scripts/GameConrtoller.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class GameConrtoller : MonoBehaviour
 {
+    public static float WorldScreenHeight { get; private set; }
+    public static float WorldScreenWidth { get; private set; }
+
+    void Awake()
+    {
+        WorldScreenHeight = Camera.main.orthographicSize * 2;
+        WorldScreenWidth = WorldScreenHeight / Screen.height * Screen.width;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
