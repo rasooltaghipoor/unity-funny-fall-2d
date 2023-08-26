3c74b1d7a8bd1e1e264f35cd33f83e02832c7b2d
rasooltaghipoor <rasool.taghipoor@gmail.com>
2023-08-26 16:43:08 +0330

    Change enemy positions during the game play

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -9,18 +9,24 @@ public class EnemySpawner : MonoBehaviour
     private const int _enemyPoolSize = 30;
     private GameObject[] _enemyPool;
     private float _lastYPos;
+    private int _currentIndex;
     // Start is called before the first frame update
     void Start()
     {
         _enemyPool = new GameObject[_enemyPoolSize];
         spawnInitialEnemies();
         _lastYPos = 0;
+        _currentIndex = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (_enemyPool[_currentIndex].transform.position.y - Camera.main.transform.position.y > GameConrtoller.WorldScreenHeight)
+        {
+            _lastYPos = Random.Range(_lastYPos - GameConrtoller.WorldScreenHeight / 2, _lastYPos - GameConrtoller.WorldScreenHeight * 2);
+            _enemyPool[_currentIndex].transform.position = new Vector3(Random.Range(-GameConrtoller.WorldScreenWidth * 0.5f, GameConrtoller.WorldScreenWidth * 0.5f), _lastYPos, 0);
+        }
     }
 
     private void spawnInitialEnemies()
@@ -29,9 +35,18 @@ public class EnemySpawner : MonoBehaviour
         {
             _enemyPool[i] = Instantiate(_enemyPrefabs[Random.Range(0, _enemyPrefabs.Length)]) as GameObject;
             _lastYPos = Random.Range(_lastYPos - GameConrtoller.WorldScreenHeight / 2, _lastYPos - GameConrtoller.WorldScreenHeight * 2);
-            _enemyPool[i].transform.position = new Vector3(Random.Range(-GameConrtoller.WorldScreenWidth * 0.75f, GameConrtoller.WorldScreenWidth * 0.75f), _lastYPos, 0);
+            _enemyPool[i].transform.position = new Vector3(Random.Range(-GameConrtoller.WorldScreenWidth * 0.5f, GameConrtoller.WorldScreenWidth * 0.5f), _lastYPos, 0);
         }
     }
 
+    private void resetEnemiesArrangement()
+    {
+        _lastYPos = 0;
+        for (int i = 0; i < _enemyPoolSize; i++)
+        {
+            _lastYPos = Random.Range(_lastYPos - GameConrtoller.WorldScreenHeight / 2, _lastYPos - GameConrtoller.WorldScreenHeight * 2);
+            _enemyPool[i].transform.position = new Vector3(Random.Range(-GameConrtoller.WorldScreenWidth * 0.5f, GameConrtoller.WorldScreenWidth * 0.5f), _lastYPos, 0);
+        }
+    }
 
 }
